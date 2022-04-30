163c141d34d66e77f575bf6a0cd9b87d5b7bc4d3
rudgns9334 <rudgns8285@naver.com>
2022-04-30 20:51:14 +0900

    spring 환경설정

---
+++ src/main/java/pelog/pelogspring/PelogSpringApplication.java
@@ -0,0 +1,13 @@
+package pelog.pelogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelogSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelogSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pelog/pelogspring/PelogSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pelog.pelogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelogSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
